556089ebf40d2e5b113099a19a9ff72ecba1c3c5
Christian <christian_haller@gmx.de>
2024-09-21 18:12:41 -0500

    # pacific/atlantic graph

--- Blind75/graphs/pacificAtlantic.py
+++ Blind75/graphs/pacificAtlantic.py
@@ -16,13 +16,75 @@ Return a 2D list of grid coordinates result where result[i] = [ri, ci] denotes t
 (ri, ci) to both the Pacific and Atlantic oceans.
 """
 
-# Accept a height map of an island, Return a list of grid coordinates on the island (diagonal crest)
-# from which water flows towards both oceans -> have lower heights than cells NW and SE.
-
 
 def pacificAtlantic(heights: list[list[int]]) -> list[list[int]]:
+    """
+    Accept a height map of an island, Return a list of grid coordinates on the island (diagonal crest)
+    from which water flows towards both oceans -> crest neighbors have lower heights than NW and SE.
 
-    return result
+    Brute force: for each grid cell, start a DFS search (which is repeated for each starting point)
+    and if successful, add to results set.
+    Time complexity: O(n*m)^2
+
+    Smarter solution: Every cell bordering Pacific can flow to Pacific. Every cell bordering Atlantic flows to Atlantic.
+    Find all non-coast cells that flow to Pacific edge. Find increasing heights and maintain a set to avoid duplicates.
+    Find all non-coast cells that flow to Atlantic edge. Find increasing heights and maintain a set to avoid duplicates.
+    Find overlap both sets of cells that can reach both.
+    Time complexity: O(n*m)
+    """
+    # Init global vars
+    ROWS, COLS = len(heights), len(heights[0])
+    # Init sets for coordinate results, global vars
+    pacset, atlset = set(), set()
+
+    # Define DFS recursive function
+    def dfs(row, col, visitSet, prevHeight):
+        """
+        Marking cell coordinates in visitSet moving uphill
+        """
+
+        # Do not write to visitSet when....
+        if (
+            ((row, col) in visitSet)  # Previously visited
+            or (row < 0 or col < 0)  # Out of bounds negative
+            or (row == ROWS or col == COLS)  # Out of bounds positive
+            or (heights[row][col] < prevHeight)
+        ):  # Height smaller, i.e., not equal or higher (we move from coast upwards)
+            return None
+
+        # Coordinates passed the if statement, add coordinates to visitSet
+        visitSet.add((row, col))
+
+        # Recursive call on four coordinate directions
+        dfs(row=row + 1, col=col, visitSet=visitSet, prevHeight=heights[row][col])  # up
+        dfs(row=row - 1, col=col, visitSet=visitSet, prevHeight=heights[row][col])  # do
+        dfs(row=row, col=col - 1, visitSet=visitSet, prevHeight=heights[row][col])  # lf
+        dfs(row=row, col=col + 1, visitSet=visitSet, prevHeight=heights[row][col])  # ri
+
+    # Run DFS on top edge row=0's cells (Pacific) and last row's cells (Atlantic), move left/right
+    # Initial 'prevHeight' coordinates are the same as starting coordinate
+    for col in range(COLS):
+        dfs(
+            row=0, col=col, visitSet=pacset, prevHeight=heights[0][col]
+        )  # top row fixed
+        dfs(
+            row=ROWS - 1, col=col, visitSet=atlset, prevHeight=heights[ROWS - 1][col]
+        )  # bottom row fixed
+
+    # Run DFS on left edge col=0' cells (Pacific) and right edge cells (Atlantic), move up/down
+    # Initial 'prevHeight' coordinates are the same as starting coordinate
+    for row in range(ROWS):
+        dfs(
+            row=row, col=0, visitSet=pacset, prevHeight=heights[row][0]
+        )  # left col fixed
+        dfs(
+            row=row, col=COLS - 1, visitSet=atlset, prevHeight=heights[row][COLS - 1]
+        )  # right col fixed
+
+    # Return overlap between pacset and atlset as list.
+    # The individual coordinate pairs are tuples instead of lists, which is also accepted.
+    # Order of coordinates also does not matter.
+    return list(pacset & atlset)
 
 
 def main():
